5d3d97c3c8bfc433f62c53707a281bb8ca96ffb7
Stephen Robinson <stephen@stephen-robinson.co.uk>
2018-07-02 10:19:52 +0100

    Add policy for datadog integration - lambda

---
+++ integrations/lambda/main.tf
@@ -0,0 +1,22 @@
+module "aws_dd_integration" {
+  source = "../../modules/aws_dd_integration"
+
+  integration_name = "Lambda"
+
+  # "lambda:List*",
+  # logs:DescribeLogGroups
+  # logs:DescribeLogStreams
+  # logs:FilterLogEvents
+  # tag:GetResources
+
+  policy_actions = [
+    "lambda:ListAliases",
+    "lambda:ListEventSourceMappings",
+    "lambda:ListFunctions",
+    "lambda:ListVersionsByFunction",
+    "logs:DescribeLogGroups",
+    "logs:DescribeLogStreams",
+    "logs:FilterLogEvents",
+    "tag:GetResources",
+  ]
+}
