e37041dc0cf9d11c4ac818726423d16b99d09f8f
Stephen Robinson <stephen@stephen-robinson.co.uk>
2018-07-02 10:18:49 +0100

    Add policy for datadog integration - code deploy

---
+++ integrations/code_deploy/main.tf
@@ -0,0 +1,22 @@
+module "aws_dd_integration" {
+  source = "../../modules/aws_dd_integration"
+
+  integration_name = "CodeDeploy"
+
+  policy_actions = [
+    "codedeploy:ListApplicationRevisions",
+    "codedeploy:ListApplications",
+    "codedeploy:ListDeploymentConfigs",
+    "codedeploy:ListDeploymentGroups",
+    "codedeploy:ListDeploymentInstances",
+    "codedeploy:ListDeployments",
+    "codedeploy:ListGitHubAccountTokenNames",
+    "codedeploy:ListOnPremisesInstances",
+    "codedeploy:BatchGetApplicationRevisions",
+    "codedeploy:BatchGetApplications",
+    "codedeploy:BatchGetDeploymentGroups",
+    "codedeploy:BatchGetDeploymentInstances",
+    "codedeploy:BatchGetDeployments",
+    "codedeploy:BatchGetOnPremisesInstances",
+  ]
+}
